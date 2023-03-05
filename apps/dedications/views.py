2e8fcd30f1c76877132d83eaf9e6664f0c7e1adf
Cristian Aguazaco <caguazaco@osx-5230.local>
2023-03-04 21:25:13 -0500

    Adjust the function and form for editing dedications

--- apps/dedications/views.py
+++ apps/dedications/views.py
@@ -9,66 +9,53 @@ from decouple import config
 from static.py.utils import billing_period, format_dates
 
 CUTOFF_DAY = config('CUTOFF_DAY', cast = int)
-[today, lower_date] = billing_period(CUTOFF_DAY)
+[lower_date, today] = billing_period(CUTOFF_DAY)
 
 # Create your views here.
 @login_required
 def home(request):
-    occupations = list_occupations(request, today)
-    tasks = list_user_active_tasks(request)
+    occupations = list_occupations(request.user, today)
+    tasks = list_user_active_tasks(request.user)
 
-    context = {
-        'cutoff_day': CUTOFF_DAY, # To share the CUTOFF_DAY environment variable with the JavaScript scripts
-        'occupations': occupations,
-        'tasks': tasks
-    }
-    
-    form = OccupationForm(request.POST or None)
-    
-    if form.is_valid():
-        form.save()
+    if request.method == 'GET':
+        context = {
+            'cutoff_day': CUTOFF_DAY, # To share the CUTOFF_DAY environment variable with the JavaScript scripts
+            'occupations': occupations,
+            'tasks': tasks
+        }
+    else:
+        form = OccupationForm(request.POST)
         
-        return redirect('home')
+        if form.is_valid():
+            form.save()
+            
+            return redirect('home')
+        else:
+            context['form'] = form
     
-    context['form'] = form
-
     return render(request, 'home.html', context)
 
 @login_required
 def record(request):
-    occupations = list_occupations(request, lower_date)
-    dates = list_unique_dates(request, lower_date)
-
+    occupations = list_occupations(request.user, lower_date)
+    
     context = {
         'cutoff_day': CUTOFF_DAY, # To share the CUTOFF_DAY environment variable with the JavaScript scripts
-        'occupations': occupations,
-        'dates': dates
+        'occupations': occupations
     }
-    
-    form = OccupationForm(request.POST or None)
-    
-    if form.is_valid():
-        form.save()
-        
-        return redirect('record')
-    
-    context['form'] = form
 
     return render(request, 'record.html', context)
 
-def list_occupations(request, day):
-    occupations = Occupation.objects.filter(users = request.user.id, date__gte = day).order_by('-date', '-start_time')
+def list_occupations(user, day):
+    occupations = Occupation.objects.filter(users = user.id, date__gte = day).order_by('-date', '-start_time')
     occupations = format_dates(occupations) # Adjusts the format of date and time fields
+
+    dates = list(set(Occupation.objects.filter(users = user.id, date__gte = day).order_by('-date').values_list('date', flat = True)))
 
     return occupations
 
-def list_unique_dates(request, day):
-    dates = list(set(Occupation.objects.filter(users = request.user.id, date__gte = day).order_by('-date').values_list('date', flat = True)))
-
-    return dates
-
-def list_user_active_tasks(request):
-    user_tasks_ids = UserTask.objects.filter(users = request.user.id, is_active = True).values_list('id', flat = True)
+def list_user_active_tasks(user):
+    user_tasks_ids = UserTask.objects.filter(users = user.id, is_active = True).values_list('tasks_id', flat = True).distinct()
     tasks = Task.objects.filter(id__in = user_tasks_ids, is_active = True)
 
     return tasks
@@ -76,18 +63,33 @@ def list_user_active_tasks(request):
 @login_required
 def edit_occupation(request, id):
     occupation = Occupation.objects.get(id = id)
-    form = OccupationForm(request.POST or None, instance = occupation)
+    selected_task = Task.objects.get(id = occupation.tasks.id)
+    other_tasks = list_user_active_tasks(request.user).exclude(id = occupation.tasks.id)
     
-    if form.is_valid():
-        form.save()
+    if request.method == 'GET':
+        form = OccupationForm(instance = occupation)
         
-        #return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
-        return redirect('home')
-
-    context = {
-        'cutoff_day': CUTOFF_DAY, # To share the CUTOFF_DAY environment variable with the JavaScript scripts
-        'form': form
-    }
+        context = {
+            'cutoff_day': CUTOFF_DAY, # To share the CUTOFF_DAY environment variable with the JavaScript scripts
+            'form': form,
+            'selected_task': selected_task,
+            'other_tasks': other_tasks
+        }
+    else:
+        form = OccupationForm(request.POST, instance = occupation)
+        
+        if form.is_valid():
+            form.save()
+            
+            # return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+            return redirect('home')
+        else:
+            context = {
+                'cutoff_day': CUTOFF_DAY, # To share the CUTOFF_DAY environment variable with the JavaScript scripts
+                'form': form,
+                'selected_task': selected_task,
+                'all_tasks': other_tasks
+            }
         
     return render(request, 'edit_occupation.html', context)
 
