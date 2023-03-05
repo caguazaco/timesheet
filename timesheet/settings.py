a84b7f57553ca9f4d5a3b734b868d1c68957f77b
Cristian Aguazaco <caguazaco@osx-5230.local>
2023-03-05 17:06:17 -0500

    Update allowed hosts

--- timesheet/settings.py
+++ timesheet/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-%47uf9&pan6&$gn)%5)6m$8xig=j9$t^xm8cmu272joox7_axv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast = lambda x: [host.strip() for host in x.split(',')])
+ALLOWED_HOSTS = ['timesheet-platform.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
